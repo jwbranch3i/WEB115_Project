4d8f62caaef80944b8a7f4acef1f2d9630f3df18
jwbranch3i <jwbranch3@hotmail.com>
2022-05-06 21:16:12 -0400

    finished and turned in

--- projectJS.js
+++ projectJS.js
@@ -1,4 +1,4 @@
-function fixDate(theDate){
+function fixDate(theDate) {
    let newDate = "";
 
    newDate = theDate.substring(5, 7) + "\\";
@@ -8,14 +8,25 @@ function fixDate(theDate){
    return newDate;
 }
 
-function doResume(){
+function checkEmail() {
+   var emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+   if (document.getElementById("email").value.match(emailformat)) {
+      doResume();
+   } else {
+      alert("Please enter a valid email address!");
+      document.getElementById("email").focus();
+   }
+}
+
+function doResume() {
    var myPage;
    let name = document.getElementById("name").value;
    let street = document.getElementById("street").value;
    let city = document.getElementById("city").value;
    let state = document.getElementById("state").value;
    let zip = document.getElementById("zip").value;
-   let phone = document.getElementById("phone" ).value;
+   let phone = document.getElementById("phone").value;
    let email = document.getElementById("email").value;
    let portFolio = document.getElementById("portFolio").value;
 
@@ -42,25 +53,24 @@ function doResume(){
    let tskill2 = document.getElementById("tskill2").value;
    let tskill3 = document.getElementById("tskill3").value;
 
-
    let references = document.getElementById("references").value;
 
-   myPage="";
-   
-   myPage = "<html lang=\"en\">";
-   myPage += "<head><meta charset=\"UTF-8\"><title>Your Resume</title>";
-   myPage += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
+   myPage = "";
 
-   myPage += "<div class=\"r_header\">";
+   myPage = '<html lang="en">';
+   myPage += '<head><meta charset="UTF-8"><title>Your Resume</title>';
+   myPage += '<link rel="stylesheet" type="text/css" href="style.css">';
+
+   myPage += '<div class="r_header">';
    myPage += "<h1><b>" + name + "</b></h1>";
-   myPage += "_______________<br><br>" ;
+   myPage += "_______________<br><br>";
    myPage += objective;
    myPage += "<hr></div></head>";
 
    myPage += "<body>";
 
-   myPage += "<div class=\"";
-   myPage += "column left\" ;>";
+   myPage += '<div class="';
+   myPage += 'column left" ;>';
    myPage += "<center><b><u>RESUME OBJECTIVE</u></b></center><br>";
    myPage += objective + ": <br>" + objDetail;
 
@@ -75,34 +85,34 @@ function doResume(){
    myPage += fixDate(startDate1) + " - " + fixDate(endDate1) + "<br>";
    myPage += work1;
 
-   if (work2 !== ""){
-   myPage += "<br><br><br>";
-   myPage += "<center><b><u>Work Experience 2</u></b></center><br>";
-   myPage += "Company:<br>";
-   myPage += fixDate(startDate2) + " - " + fixDate(endDate2) + "<br>";
-   myPage += work2;
+   if (work2 !== "") {
+      myPage += "<br><br><br>";
+      myPage += "<center><b><u>Work Experience 2</u></b></center><br>";
+      myPage += "Company:<br>";
+      myPage += fixDate(startDate2) + " - " + fixDate(endDate2) + "<br>";
+      myPage += work2;
    }
-   
-   if (work3 !== ""){
-   myPage += "<br><br><br>";
-   myPage += "<center><b><u>Work Experience 3</u></b></center><br>";
-   myPage += "Company:<br>";
-   myPage += "startdate - enddate<br>";
-   myPage += fixDate(startDate3) + " - " + fixDate(endDate3) + "<br>";
-   myPage += work3;
+
+   if (work3 !== "") {
+      myPage += "<br><br><br>";
+      myPage += "<center><b><u>Work Experience 3</u></b></center><br>";
+      myPage += "Company:<br>";
+      myPage += "startdate - enddate<br>";
+      myPage += fixDate(startDate3) + " - " + fixDate(endDate3) + "<br>";
+      myPage += work3;
    }
-   
+
    myPage += "<br><br><br>";
    myPage += "<center><b><u>References</u></b></center><br>";
    myPage += "Name:<br>";
    myPage += references + "</div>";
 
    myPage += "<div class=";
-   myPage += "\"column right\" ;>";
+   myPage += '"column right" ;>';
    myPage += email + "<br>";
    myPage += phone + "<br>";
-   myPage += street +"<br>";
-   myPage += city + ", " + state +"<br>" + zip + "<br>";
+   myPage += street + "<br>";
+   myPage += city + ", " + state + "<br>" + zip + "<br>";
    myPage += portFolio;
 
    myPage += "<h2>SKILLS</h2>";
@@ -115,9 +125,8 @@ function doResume(){
    myPage += tskill2 + "<br>";
    myPage += tskill3 + "<br>";
 
+   myPage += "</div></body></html>";
 
-   myPage += "</div></body></html>"
-   
-   newWindow = window.open("", "Resume", "width=400", "height= 400");
+   var newWindow = window.open("", "Resume", "width=400", "height= 400");
    newWindow.document.write(myPage);
 }
